10969015c07d15b565e508fd0ea977c6c5ab4533
Leo0114 <leonel0614@gmail.com>
2023-01-25 17:10:39 -0600

    astro 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://Leo0114.github.io',
-  base: '/astro-landingpage',
+  base: '/astro-landingpage'
 });
